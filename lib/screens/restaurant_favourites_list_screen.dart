45b5ce2513091aabce3bdd28e52a9b72637dee53
marco <m.zirkenbach@gmx.de>
2020-01-30 18:23:57 +0100

    adapt favorite screen to new style

--- lib/screens/restaurant_favourites_list_screen.dart
+++ lib/screens/restaurant_favourites_list_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:lunch_app/providers/general_info.dart';
-import 'package:lunch_app/widgets/foodCategoryRestaurantFilter.dart';
 import 'package:lunch_app/widgets/title_section.dart';
 import 'package:lunch_app/widgets/weekday_navigation_bar.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +8,7 @@ import 'package:provider/provider.dart';
 import '../date_util.dart';
 import '../widgets/restaurants_list.dart';
 
+// TODO: reuse RestaurantListScreen and do not copy favorite screen
 class RestaurantsFavouritesListScreen extends StatefulWidget {
   final void Function(int) navigateTo;
 
@@ -30,11 +30,11 @@ class _RestaurantsFavouritesListScreenState extends State<RestaurantsFavouritesL
     super.initState();
   }
 
-  Future<Position> _getLocation() async {
-    var currentLocation;
+  Future<Position> _getLocation() {
+    Future<Position> currentLocation;
     try {
-      currentLocation = await geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best);
+      currentLocation =
+          geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
     } catch (e) {
       currentLocation = null;
     }
@@ -44,11 +44,10 @@ class _RestaurantsFavouritesListScreenState extends State<RestaurantsFavouritesL
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      _getLocation().then((value) async {
-        setState(() {
-          currentPosition = value;
-        });
+      _getLocation().then((value) {
+        currentPosition = value;
       });
+
       var todayDateTime = DateTime.now();
       weekdays = DateUtil.getWeekDaysForDate(todayDateTime);
     }
@@ -64,17 +63,16 @@ class _RestaurantsFavouritesListScreenState extends State<RestaurantsFavouritesL
           backgroundColor: Colors.white,
           title: Center(child: TitleSection()),
           bottom: PreferredSize(
-            preferredSize: Size.fromHeight(150),
+            preferredSize: Size.fromHeight(120),
             child: Expanded(
               flex: 2,
               child: Container(
                 constraints: BoxConstraints.expand(),
                 child: weekdays != null && weekdays.length > 0
                     ? WeekDayNavigationBar(
-                        weekdays,
-                        Provider.of<GeneralInfo>(context).currentWeekdayIndex,
-                        widget.navigateTo
-                )
+                    weekdays,
+                    Provider.of<GeneralInfo>(context).currentWeekdayIndex,
+                    widget.navigateTo)
                     : Container(),
               ),
             ),
@@ -83,20 +81,29 @@ class _RestaurantsFavouritesListScreenState extends State<RestaurantsFavouritesL
         body: Container(
           color: Colors.white,
           child: !Provider.of<GeneralInfo>(context).isLoadingRestaurants
-            ? Container(
+              ? Container(
             // TODO: check how to set background color globally
             child: Column(
               children: <Widget>[
-                AnimatedSize(
-                  vsync: this,
-                  duration: Duration(milliseconds: 200),
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: FoodCategoryRestaurantFilter((t) {}, []), // TODO: fix this
-                  ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15.0, right: 15.0, bottom: 18.0, top: 18.0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'FAVORITEN RESTAURANTS \nIN DEINER NÄHE',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ]),
                 ),
                 Expanded(
-                  child: RestaurantsList(Provider.of<GeneralInfo>(context).currentWeekdayIndex, currentPosition, true),
+                  child: RestaurantsList(
+                      Provider.of<GeneralInfo>(context)
+                          .currentWeekdayIndex,
+                      currentPosition,
+                      true),
                 ),
               ],
             ),
